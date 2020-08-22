f424b8a4b7fe210e17563975ea42630186ffc023
mithem <mithem@github.com>
2020-08-22 10:57:03 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Miguel Themann on 22.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizerApp.swift
+//  Shared
+//
+//  Created by Miguel Themann on 22.08.20.
+//
+
+import SwiftUI
+
+@main
+struct OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
