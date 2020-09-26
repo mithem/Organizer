a89bb24c475c7887aebba48b4bfccb7307955489
mithem <mithem@github.com>
2020-09-26 08:57:38 +0200

    remove AppDelegate.swift & breakpoints

--- UI/OrganizerApp.swift
+++ UI/OrganizerApp.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 @main
 struct OrganizerApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     var body: some Scene {
         WindowGroup {
             ContentView()
